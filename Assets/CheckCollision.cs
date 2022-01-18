e06f8279c9c9825d0ecc7f1b4d533aa721f481fa
Khang <87509334+perckgg@users.noreply.github.com>
2022-01-18 19:54:47 +0700

    Add files via upload

--- Assets/CheckCollision.cs
+++ Assets/CheckCollision.cs
@@ -1,9 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class CheckCollision : MonoBehaviour
 {   public Movement fish_moving;
+    public GameManager Manager;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Rubbish"))
@@ -12,6 +13,7 @@ public class CheckCollision : MonoBehaviour
         }
         else if(collision.CompareTag("BakaFish")){
             fish_moving.enabled=false;
+            FindObjectOfType<GameManager>().EndGame();
         }
     }
 }
